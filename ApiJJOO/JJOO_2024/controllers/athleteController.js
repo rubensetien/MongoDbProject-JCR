53e5f7400b6d628d4c588716701e28bb1a7f90df
jorgeog25 <jrotero258@gmail.com>
2024-04-04 14:26:10 +0200

    Implementacion del Buscador en todas las opciones. Creacion de los html de busqueda e implementacion de desplegable del buscador

--- ApiJJOO/JJOO_2024/controllers/athleteController.js
+++ ApiJJOO/JJOO_2024/controllers/athleteController.js
@@ -11,6 +11,9 @@ exports.searchAthlete = async (req, res) => {
         // Si el campo es 'age' y q es un número, buscar por edad exacta
         if (field === 'age' && isNumber) {
             query[field] = q;
+        } else if (field === 'medals' && isNumber) {
+            // Si el campo es 'medals' y q es un número, buscar por cantidad exacta de medallas
+            query[field] = q;
         } else {
             // De lo contrario, buscar como texto
             query[field] = { $regex: q, $options: 'i' };
@@ -24,4 +27,14 @@ exports.searchAthlete = async (req, res) => {
         console.error('Error searching athletes:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-};
+}
+
+exports.getAllAthlete = async (req, res) => {
+    try {
+        const athletes = await Athlete.find();
+        res.render('athlete', { athletes });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Failed to retrieve athletes');
+    }
+}
