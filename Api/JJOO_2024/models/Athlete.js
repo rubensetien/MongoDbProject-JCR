098704f2002139a3209e382cb33af1a73699ca70
jorgeog25 <jrotero258@gmail.com>
2024-04-11 16:03:06 +0200

    Implementacion de los indices

--- Api/JJOO_2024/models/Athlete.js
+++ Api/JJOO_2024/models/Athlete.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
 const athleteSchema = new mongoose.Schema({
-
-    name: {type: String},
-    country: {type: String},
-    team: {type: String},
-    age: {type: Number},
-    gender : {type: String},
-    modality: {type: String},
-    medals: {type: Number},
-    record: {type: String},
-    picture: {type: String}
-
+    name: { type: String },
+    country: { type: String, index: true }, // Agregando un índice al campo 'country'
+    team: { type: String },
+    age: { type: Number },
+    gender: { type: String },
+    modality: { type: String },
+    medals: { type: Number },
+    record: { type: String },
+    picture: { type: String }
 });
+
 const Athlete = mongoose.model('Athlete', athleteSchema);
 module.exports = Athlete;
